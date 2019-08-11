7dddd05e749eadda0ead76cb4ee12cf80d38517f
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-08-11 20:32:26 +0900

    feat: トップページの仮作成

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
+import { Container, Image, Menu, Button, Icon, Header, Grid } from 'semantic-ui-react';
 
 import { Props } from '../../containers/App';
 
-import logo from './logo.svg';
-import './App.css';
+import './primary.css';
+import logo_header from './logo_header.png';
+import logo from './logo.png';
 
 const App: React.FC<Props> = props => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <input value={props.appState.name} onChange={e => props.changeName(e.target.value)} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu fixed='top' color='yellow' inverted>
+        <Menu.Item as='a' header fitted>
+          <Image size='small' src={logo_header} />
+        </Menu.Item>
+      </Menu>
+
+      <Container text style={{ paddingTop: '3em' }}>
+        <Image src={logo} />
+
+        <Grid container verticalAlign='middle'>
+          <Grid.Row stretched>
+            <Grid.Column width='8'>
+              <Header>あなたのコミットを共有しませんか？</Header>
+              <p>Commitlyはあなたの書いたコードを言語別に集計して共有するためのサービスです</p>
+            </Grid.Column>
+            <Grid.Column width='8'>
+              <a className='twitter-timeline' data-height='400' href='https://twitter.com/commitly_jp' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+        <Button color='black'>
+          <Icon name='github' /> Sign in with GitHub
+        </Button>
+      </Container>
     </div>
   );
 };
