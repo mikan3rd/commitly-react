684652a524d3e285743becb0d034c092c8866a69
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-08-14 00:01:07 +0900

    fix: gitの整理2

---
+++ src/components/MyPage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Props } from '../../containers/MyPage';
+
+const MyPage: React.FC<Props> = props => {
+  return <div>MyPage</div>;
+};
+
+export default MyPage;
